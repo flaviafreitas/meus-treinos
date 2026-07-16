4cc1fb3639e1735d192c47c3d0e861c9c6cbd66f
Flávia Freitas <147860585+flaviafreitas@users.noreply.github.com>
2026-07-16 15:54:14 -0300

    Shrink add button on exercise picker

--- src/components/BibliotecaExercicios.jsx
+++ src/components/BibliotecaExercicios.jsx
@@ -12,7 +12,7 @@ function IconSearch() {
 
 function IconPlus() {
   return (
-    <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
       <path d="M12 5v14M5 12h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
     </svg>
   )
